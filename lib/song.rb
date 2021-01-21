b57632fbe68caf4fa09ffb6cf6b69079d5a2ecb7
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-21 15:25:07 -0800

    commit tree 310ce8d40038f761fdc5e9679336670966654762

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,67 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name=(name)
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name=(name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+def self.find_or_create_by_name(name)
+  if !self.find_by_name(name) == false
+    self.find_by_name(name) 
+  else
+    self.create_by_name(name) 
+  end
+end
+
+def self.alphabetical
+  self.all.sort_by {|song| song.name}
+end
+
+def self.new_from_filename(filename)
+  array = filename.split(/\ - |\./)
+  array.pop
+  artist_name = array[0]
+  name = array[1]
+  song = self.new
+  song.artist_name= artist_name
+  song.name= name
+  song
+end
+
+def self.create_from_filename(filename)
+  array = filename.split(/\ - |\./)
+  array.pop
+  artist_name = array[0]
+  name = array[1]
+  song = self.new
+  song.artist_name= artist_name
+  song.name= name
+  song
+  self.all << song
+end
+
+def self.destroy_all
+  self.all.clear
+end
 
 end
